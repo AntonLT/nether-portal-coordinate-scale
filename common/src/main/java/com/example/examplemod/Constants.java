7849cc6b20c26179f7de97152f486c48ad418cbe
AntonLT <antonlteacup@gmail.com>
2025-06-10 16:43:03 +0300

    WIP template

---
+++ common/src/main/java/com/example/examplemod/Constants.java
@@ -0,0 +1,11 @@
+package com.example.examplemod;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "netherportalcoordinatescale";
+	public static final String MOD_NAME = "ExampleMod";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
